62df1ccf800627bc4e7b0388ba7e7dfd0691fe54
Carlos Aragon <carlos.aragon@ipea.gov.br>
2018-09-21 16:50:19 -0300

    3 - barra vermelha por novo teste para a mesma unidade previamente implementada

--- src/CDBTeste.java
+++ src/CDBTeste.java
@@ -12,15 +12,33 @@ class CDBTeste {
 	}
 	
 	@Test
+	void  OutroRendimentoBrutoTeste() {
+		CDB cdb = new CDB(120, 500.00, 8.0, 22.5);
+		assertEquals(13.15, cdb.calculaRendimentoBruto());
+	}
+	
+	@Test
 	void  ImpostoDeRendaTeste() {
 		CDB cdb = new CDB(60, 1000.00, 8.5, 22.5);
 		assertEquals(3.14, cdb.calculaImpostoDeRenda());
 	}
 	
 	@Test
+	void  OutroImpostoDeRendaTeste() {
+		CDB cdb = new CDB(120, 500.00, 8.0, 22.5);
+		assertEquals(2.96, cdb.calculaImpostoDeRenda());
+	}
+	
+	@Test
 	void  RendimentoLiquidoTeste() {
 		CDB cdb = new CDB(60, 1000.00, 8.5, 22.5);
 		assertEquals(1.0829, cdb.calculaRendimentoLiquido());
+	}
+	
+	@Test
+	void  OutroRendimentoLiquidoTeste() {
+		CDB cdb = new CDB(120, 500.00, 8.0, 22.5);
+		assertEquals(2.0384, cdb.calculaRendimentoLiquido());
 	}
 
 }
